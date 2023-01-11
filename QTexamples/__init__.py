3622dcc08556ae6dae3c0256feb4884d415a4634
EVANGELOS TIKAS <49168031+EvangelosTikas@users.noreply.github.com>
2023-01-11 10:54:47 +0200

    create __init__.py

---
+++ QTexamples/__init__.py
@@ -0,0 +1,12 @@
+# import os
+
+# from sqlalchemy.orm import sessionmaker
+# from sqlalchemy import create_engine
+
+# engine = create_engine(os.environ['DATABASE_URL'])
+# Session = sessionmaker(bind=engine)
+
+# ## __init__ example
+
+
+# from <smth> import <smth.included>
